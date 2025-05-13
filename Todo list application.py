88b7a5a2af406787581f5112b674b03259f0cf1b
reham <rnasr3667@gmail.com>
2025-05-13 09:53:50 +0300

    add new option 'mark task as completed'

--- Todo list application.py
+++ Todo list application.py
@@ -1,12 +1,13 @@
-tasks = ["study english", "walking"]
+tasks = [{"task": "study english", "status": "done"},
+          {"task": "walking", "status": "pending"}]
 
 
 def View_Tasks():
     if not tasks:
         print("no tasks to display")
     else:
-        for i, task in enumerate(tasks, 1):
-            print(f'{i}-{task}')
+        for i, t in enumerate(tasks, 1):
+            print(f'{i}-{t["task"]}, {t["status"]}')
 
 
 def Add_a_Task():
@@ -18,13 +19,15 @@ def Add_a_Task():
 def Remove_a_task(task_number):
     del tasks[task_number - 1]
     print("done :) ")
-
+def mark_done(task_number):
+    tasks[task_number-1]["status"] = "done"
 
 print('''Todo List Menu:
  1. View Tasks
  2. Add a Task
  3. Remove a Task
- 4. Exit''')
+ 4. Mark Done
+ 5. Exit''')
 while True:
     choice = input("Enter your choice: ")
     if choice == '1':
@@ -34,5 +37,8 @@ while True:
     elif choice == '3':
         task_number = int(input("Enter the number of task you want to remove : "))
         Remove_a_task(task_number)
+    elif choice == '4':
+        task_number = int(input("Enter the number of task that finished : "))
+        mark_done(task_number)
     else:
         break
