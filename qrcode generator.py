bf786c46967431a231dbccba86ee2b9c327a9a45
reham nasr <rn1136@fayoum.edu.eg>
2025-06-03 02:35:04 +0300

    version2 of qrcode project

---
+++ qrcode generator.py
@@ -0,0 +1,6 @@
+import qrcode
+data = input("enter the text or URL for the QR code: ")
+fileName = input ("enter the filename: ")
+image = qrcode.make(data)
+image.save(fileName )
+print(f"QR code saved as {fileName}.")
