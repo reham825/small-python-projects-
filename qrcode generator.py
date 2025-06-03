6b0e9c31b38f638f7c1e1ba085ff208eddd733aa
reham nasr <rn1136@fayoum.edu.eg>
2025-06-03 03:53:26 +0300

    add color for qrcode

--- qrcode generator.py
+++ qrcode generator.py
@@ -1,13 +1,8 @@
 import qrcode
-<<<<<<< HEAD
 data = input("enter the text or URL for the QR code: ")
 fileName = input ("enter the filename: ")
 image = qrcode.make(data)
 image.save(fileName )
 print(f"QR code saved as {fileName}.")
-=======
-data = input("Enter the text or url for the qr code: ")
-image = qrcode.make(data)
-image.save("qrcodeimage.png")
 
->>>>>>> 5523eefd482ed967212cb36ea099b9b95f792869
+
