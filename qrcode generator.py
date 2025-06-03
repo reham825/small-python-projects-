73d8a83b6d31149c6678063c607d27484d75f452
reham nasr <rn1136@fayoum.edu.eg>
2025-06-03 04:04:17 +0300

    v3

--- qrcode generator.py
+++ qrcode generator.py
@@ -1,8 +1,13 @@
 import qrcode
+qr = qrcode.QRCode(box_size=10, border= 5)
 data = input("enter the text or URL for the QR code: ")
 fileName = input ("enter the filename: ")
-image = qrcode.make(data)
+qr.add_data(data)
+qr.make(fit =True)
+image = qr.make_image(fill_color= "red", back_color="black")
 image.save(fileName )
-print(f"QR code saved as {fileName}.")
+# image = qrcode.make(data)
+#
+# print(f"QR code saved as {fileName}.")
 
 
